13d3752f97d98df3724f48c9a2cdb60b6e434710
Rishith573 <77111082+Rishith573@users.noreply.github.com>
2021-06-03 14:34:48 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "SPONGE";
+const accessCode2 = "AGE";
+const accessCode3 = "CLOCK";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("P O N S G E", 100,50)
+    fill("lightblue")
+    text("Hint: What is full of holes but still holds water?", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("E G A", 700,150)
+    fill("lightblue")
+    text("Hint: What goes up but never comes down?", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("K C C L O", 100,250)
+    fill("lightblue")
+    text("Hint: What has hands, but can’t clap?", 100,270)
+
+}
